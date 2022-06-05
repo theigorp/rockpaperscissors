d8b5a9440d86a05fd7989fb3c94875822a41112d
Igor <mr.igor187@gmail.com>
2022-06-05 08:42:35 +0200

    make an array with choices and write a random index generator

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("hi");
+function computerPlay()
+{
+    const choice = ["Rock", "Paper", "Scissors"];
+
+    //generate a random index 0-2 using math.floor(returns largest int less than or equal to given number),
+    //and math.radnom which generates a num from 0-1(not including 1)
+    let randomNum = Math.floor(Math.random() * 3);
+}
