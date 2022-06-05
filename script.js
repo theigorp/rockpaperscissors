1a2bbc8d5661cc3ec7d8f1b7eed75c76462d15e7
Igor <mr.igor187@gmail.com>
2022-06-05 09:09:39 +0200

    write singleRound function which determines winner based on playerSelection and computerSelection

--- script.js
+++ script.js
@@ -4,5 +4,24 @@ function computerPlay()
 
     //generate a random index 0-2 using math.floor(returns largest int less than or equal to given number),
     //and math.radnom which generates a num from 0-1(not including 1)
-    let randomNum = Math.floor(Math.random() * 3);
+    let randomIndex = Math.floor(Math.random() * 3);
+
+    return choice[randomIndex];
+}
+
+let playerSelection = prompt("Rock, paper or scissors?");
+let computerSelection = computerPlay();
+
+function singleRound(playerSelection, computerSelection)
+{
+    if (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'Scissors') return "Player wins - rock beats scissors";
+    else if (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'Paper') return "Player loses - paper beats rock";
+    else if (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'Rock') return "Draw";
+    else if (playerSelection.toUpperCase() == 'Paper' && computerSelection == 'Rock') return "Player wins - paper beats rock";
+    else if (playerSelection.toUpperCase() == 'Paper' && computerSelection == 'Scissors') return "Player loses - scissors beat paper";
+    else if (playerSelection.toUpperCase() == 'Paper' && computerSelection == 'Paper') return "Draw";
+    else if (playerSelection.toUpperCase() == 'Scissors' && computerSelection == 'Scissors') return "Draw";
+    else if (playerSelection.toUpperCase() == 'Scissors' && computerSelection == 'Rock') return "Player loses - scissors beat rock";
+    else if (playerSelection.toUpperCase() == 'Scissors' && computerSelection == 'Paper') return "Player wins - scissors beat paper";
+    else return "error";
 }
