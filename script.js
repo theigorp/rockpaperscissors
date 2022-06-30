c8843bf71ae7e1f00d12242bec30bd99a750d413
Igor <mr.igor187@gmail.com>
2022-06-30 09:41:58 +0200

    ne znam sta je ovo

--- script.js
+++ script.js
@@ -11,44 +11,123 @@ function computerPlay()
 
 let playerSelection;
 let computerSelection;
+let rounds=0;
 
 //singleRound == playRound
 function singleRound(playerSelection, computerSelection)
 {
     //1 is rock, 2 is paper, 3 is scissors
-    if (playerSelection == 1 && computerSelection == 'Scissors') return "Computer selected scissors\nPlayer wins - rock beats scissors";
-    else if (playerSelection == 1 && computerSelection == 'Paper') return "Computer selected Paper\nPlayer loses - paper beats rock";
-    else if (playerSelection == 1 && computerSelection == 'Rock') return "Computer selected rock\nDraw";
-    else if (playerSelection == 2 && computerSelection == 'Rock') return "Computer selected rock\nPlayer wins - paper beats rock";
-    else if (playerSelection == 2 && computerSelection == 'Scissors') return "Computer selected scissors\nPlayer loses - scissors beat paper";
-    else if (playerSelection == 2 && computerSelection == 'Paper') return "Computer selected paper\nDraw";
-    else if (playerSelection == 3 && computerSelection == 'Scissors') return "Computer selected scissors\nDraw";
-    else if (playerSelection == 3 && computerSelection == 'Rock') return "Computer selected rock\nPlayer loses - scissors beat rock";
-    else if (playerSelection == 3 && computerSelection == 'Paper') return "Computer selected paper\nPlayer wins - scissors beat paper";
+    if (playerSelection == 1 && computerSelection == 'Scissors') return "Player wins";
+    else if (playerSelection == 1 && computerSelection == 'Paper') return "Player loses";
+    else if (playerSelection == 1 && computerSelection == 'Rock') return "Draw";
+    else if (playerSelection == 2 && computerSelection == 'Rock') return "Player wins";
+    else if (playerSelection == 2 && computerSelection == 'Scissors') return "Player loses";
+    else if (playerSelection == 2 && computerSelection == 'Paper') return "Draw";
+    else if (playerSelection == 3 && computerSelection == 'Scissors') return "Draw";
+    else if (playerSelection == 3 && computerSelection == 'Rock') return "Player loses";
+    else if (playerSelection == 3 && computerSelection == 'Paper') return "Player wins";
     else return "error";
 }
 
-const userChoice = document.querySelector('.user-choice');
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissors = document.querySelector('#scissors');
+const playerRock = document.querySelector('.player-rock');
+const playerPaper = document.querySelector('.player-paper');
+const playerScissors = document.querySelector('.player-scissors');
+const computerRock = document.querySelector('.computer-rock');
+const computerPaper = document.querySelector('.computer-paper');
+const computerScissors = document.querySelector('.computer-scissors');
 
-const results = document.querySelector('.results');
-const finalResult = document.createElement('p');
-results.appendChild(finalResult);
+const playerScoreElement = document.querySelector('.player-score');
+const computerScoreElement = document.querySelector('.computer-score');
 
-rock.addEventListener('click', () => {
+let playerScore = 0;
+let computerScore = 0;
+
+playerRock.addEventListener('click', () => {
+    playerScissors.style.backgroundColor = '';
+    playerPaper.style.backgroundColor = '';
+    computerRock.style.backgroundColor = '';
+    computerScissors.style.backgroundColor = '';
+    computerPaper.style.backgroundColor = '';
+
     playerSelection = 1;
-    finalResult.textContent = singleRound(playerSelection, computerPlay());
+    computerSelection = computerPlay();
+
+    switch(singleRound(playerSelection, computerSelection))
+    {
+        case 'Player wins':
+            playerRock.style.backgroundColor = 'green';
+            computerScissors.style.backgroundColor = 'red';
+            playerScore++;
+            playerScoreElement.textContent = `SCORE: ${playerScore}`;
+            break;
+        case 'Player loses':
+            playerRock.style.backgroundColor = 'red';
+            computerPaper.style.backgroundColor = 'green';
+            computerScore++;
+            computerScoreElement.textContent = `SCORE: ${computerScore}`;
+            break;
+        case 'Draw':
+            playerRock.style.backgroundColor = 'blue';
+            computerRock.style.backgroundColor = 'blue';
+    }
 });
 
-paper.addEventListener('click', () => {
+playerPaper.addEventListener('click', () => {
+    playerScissors.style.backgroundColor = '';
+    playerRock.style.backgroundColor = '';
+    computerRock.style.backgroundColor = '';
+    computerScissors.style.backgroundColor = '';
+    computerPaper.style.backgroundColor = '';
+
     playerSelection = 2;
-    finalResult.textContent = singleRound(playerSelection, computerPlay());
+    computerSelection = computerPlay();
+
+    switch(singleRound(playerSelection, computerSelection))
+    {
+        case 'Player wins':
+            playerPaper.style.backgroundColor = 'green';
+            computerRock.style.backgroundColor = 'red';
+            playerScore++;
+            playerScoreElement.textContent = `SCORE: ${playerScore}`;
+            break;
+        case 'Player loses':
+            playerPaper.style.backgroundColor = 'red';
+            computerScissors.style.backgroundColor = 'green';
+            computerScore++;
+            computerScoreElement.textContent = `SCORE: ${computerScore}`;
+            break;
+        case 'Draw':
+            playerPaper.style.backgroundColor = 'blue';
+            computerPaper.style.backgroundColor = 'blue';
+    }
 });
 
-scissors.addEventListener('click', () => {
-    playerSelection = 3;
+playerScissors.addEventListener('click', () => {
+    playerPaper.style.backgroundColor = '';
+    playerRock.style.backgroundColor = '';
+    computerRock.style.backgroundColor = '';
+    computerScissors.style.backgroundColor = '';
+    computerPaper.style.backgroundColor = '';
 
-    finalResult.textContent = singleRound(playerSelection, computerPlay());
+    playerSelection = 3;
+    computerSelection = computerPlay();
+
+    switch(singleRound(playerSelection, computerSelection))
+    {
+        case 'Player wins':
+            playerScissors.style.backgroundColor = 'green';
+            computerPaper.style.backgroundColor = 'red';
+            playerScore++;
+            playerScoreElement.textContent = `SCORE: ${playerScore}`;
+            break;
+        case 'Player loses':
+            playerScissors.style.backgroundColor = 'red';
+            computerRock.style.backgroundColor = 'green';
+            computerScore++;
+            computerScoreElement.textContent = `SCORE: ${computerScore}`;
+            break;
+        case 'Draw':
+            playerScissors.style.backgroundColor = 'blue';
+            computerScissors.style.backgroundColor = 'blue';
+    }
 });
